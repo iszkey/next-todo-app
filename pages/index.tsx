df2281356154b5db0f0629048428a972271a6087
iszkey <kisozaki1026@gmail.com>
2023-02-04 21:07:08 +0900

    ログインページの機能を実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,32 @@
+import { useState, FormEvent } from 'react';
+import { BadgeCheckIcon, ShieldCheckIcon } from '@heroicons/react/solid';
 import { Layout } from '@/components/Layout';
+import { NextPage } from 'next';
+import { useMutateAuth } from '@/hooks/useMutateAuth';
 
-export default function Home() {
+const Auth: NextPage = () => {
+    const [isLogin, setIsLogin] = useState(true);
+
+    const {
+        email,
+        setEmail,
+        password,
+        setPassword,
+        loginMutation,
+        registerMutation,
+    } = useMutateAuth();
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        // submit によるページのりろー度を防ぐ
+        e.preventDefault();
+
+        if (isLogin) {
+            loginMutation.mutate();
+        } else {
+            registerMutation.mutate();
+        }
+    };
     return <Layout title="Home">{'hello'}</Layout>;
-}
+};
+
+export default Auth;
