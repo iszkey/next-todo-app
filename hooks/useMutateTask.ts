f12b8c01761897515f042631d27b10e8b08ddf59
iszkey <kisozaki1026@gmail.com>
2023-02-08 23:34:47 +0900

    Taskをupdate, createするmutationの作成

---
+++ hooks/useMutateTask.ts
@@ -0,0 +1,72 @@
+import { useQueryClient, useMutation } from 'react-query';
+import { useStore } from '@/store';
+import { supabase } from '@/utils/supabase';
+import { Task, EditedTask } from '@/types/types';
+
+export const useMutateTask = () => {
+    const queryClient = useQueryClient();
+    const reset = useStore((state) => state.resetEditedTask);
+
+    const createTaskMutation = useMutation(
+        async (task: Omit<Task, 'id' | 'created_at'>) => {
+            const { data, error } = await supabase.from('todos').insert(task);
+            if (error) throw new Error(error.message);
+            return data;
+        },
+        {
+            onSuccess: (res) => {
+                // 'todos'というkeyに紐づくキャッシュデータを取得する
+                const previousTodos = queryClient.getQueryData<Task[]>([
+                    'todos',
+                ]);
+
+                if (previousTodos) {
+                    // キャッシュデータの更新
+                    queryClient.setQueryData(
+                        ['todos'],
+                        [...previousTodos, res[0]] //スプレット演算子で展開して末尾に新しいtodoを追加する
+                    );
+                }
+
+                // EditedTaskを初期化する
+                reset();
+            },
+            onError: (err: any) => {
+                alert(err.message);
+                reset();
+            },
+        }
+    );
+
+    const updateTaskMutation = useMutation(
+        async (task: EditedTask) => {
+            const { data, error } = await supabase
+                .from('todos')
+                .update({ title: task.title })
+                .eq('id', task.id);
+            if (error) throw new Error(error.message);
+            return data;
+        },
+        {
+            // variablesはmutationに渡した引数(EditedTask)
+            onSuccess: (res, variables) => {
+                const previousTodos = queryClient.getQueryData<Task[]>([
+                    'todos',
+                ]);
+                if (previousTodos) {
+                    queryClient.setQueryData(
+                        ['todos'],
+                        previousTodos.map((task) => {
+                            task.id === variables.id ? res[0] : task;
+                        })
+                    );
+                }
+                reset();
+            },
+            onError: (err: any) => {
+                alert(err.message);
+                reset();
+            },
+        }
+    );
+};
