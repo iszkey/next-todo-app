49fbf49c8a58b118266614f6ae135ed2d0cda392
iszkey <kisozaki1026@gmail.com>
2023-02-08 22:15:34 +0900

    export を設定

--- store.ts
+++ store.ts
@@ -1,44 +1,43 @@
-import create from "zustand/react";
-import { EditedTask, EditedNotice } from "./types/types";
+import create from 'zustand/react';
+import { EditedTask, EditedNotice } from './types/types';
 
 /**
-* Zustandでグローバルステートの状態管理をするために、
-* 最初に管理するオブジェクトと更新用の関数をまとめて定義する。
-**/
+ * Zustandでグローバルステートの状態管理をするために、
+ * 最初に管理するオブジェクトと更新用の関数をまとめて定義する。
+ **/
 
 //返り値は特にない場合はvoidを指定する
 
 /** 管理するオブジェクトと関数の型定義 */
 type State = {
-    editedTask: EditedTask
-    editedNotice: EditedNotice
-    updatedEditedTask:(payload: EditedTask) => void 
-    updatedEditedNotice:(payload:EditedNotice) => void
-    resetEditedTask: () => void
-    resetEditedNotice: () => void
-}
+    editedTask: EditedTask;
+    editedNotice: EditedNotice;
+    updatedEditedTask: (payload: EditedTask) => void;
+    updatedEditedNotice: (payload: EditedNotice) => void;
+    resetEditedTask: () => void;
+    resetEditedNotice: () => void;
+};
 
 /** Zustandのcreateメソッドでステートと更新用の関数を定義する */
-const useStore = create<State>((set) =>({
-    editedTask:{id:'',title:''},
-    editedNotice:{id:'',content:''},
-    updatedEditedTask:(payload) => {
+export const useStore = create<State>((set) => ({
+    editedTask: { id: '', title: '' },
+    editedNotice: { id: '', content: '' },
+    updatedEditedTask: (payload) => {
         set({
-            editedTask:{
-                id:payload.id,
-                title:payload.title
-            }
-        })
-    },
-    updatedEditedNotice:(payload) => {
-        set({
-            editedNotice:{
-                id:payload.id,
-                content:payload.content
+            editedTask: {
+                id: payload.id,
+                title: payload.title,
             },
-        })
+        });
     },
-    resetEditedTask:() => set({editedTask:{id:'',title:''}}),
-    resetEditedNotice:()=>({editedNotice:{id:'', content:''}})
-}))
-
+    updatedEditedNotice: (payload) => {
+        set({
+            editedNotice: {
+                id: payload.id,
+                content: payload.content,
+            },
+        });
+    },
+    resetEditedTask: () => set({ editedTask: { id: '', title: '' } }),
+    resetEditedNotice: () => ({ editedNotice: { id: '', content: '' } }),
+}));
